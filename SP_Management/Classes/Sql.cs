cac8156cd712421b97811ea660c5e79f466a9913
Atippaz <nasakun13201@gmail.com>
2022-05-21 00:54:49 +0700

    import sql

---
+++ SP_Management/Classes/Sql.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.SqlClient;
+
+namespace SP_Management.Classes
+{
+    public static class Sql
+    {
+        static SqlConnection _sqlConnection = null;
+        static string _sqlConnectionString = "";
+        static SqlCommand _command = null;
+        static SqlDataAdapter _adapter = null;
+        public static void SqlConnectionOpen() {
+            _sqlConnection = new SqlConnection(_sqlConnectionString);
+            _sqlConnection.Open();
+        }
+        public static void SqlConnectionClose() {
+             _sqlConnection.Close();
+             _sqlConnection.Dispose();
+        }
+        public static void RunCommand(string cmd)
+        {
+            _command = new SqlCommand(cmd,_sqlConnection);
+        }
+    }
+}
