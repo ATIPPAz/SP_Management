56d8dcaf854cc4006a224fd64e641ea3fcfede05
Atippaz <nasakun13201@gmail.com>
2022-05-21 09:09:23 +0700

    Create menu button list

--- SP_Management/Pages/IndexPage/SPM.cs
+++ SP_Management/Pages/IndexPage/SPM.cs
@@ -12,11 +12,13 @@ namespace SP_Management
 {
     public partial class SPM : Form
     {
-
+        string ClickData = "";
         public SPM()
         {
             InitializeComponent();
-           
+            resizeMenuPanel();
+            CloseAllSubMenu();
+            
         }
 
      
@@ -43,19 +45,131 @@ namespace SP_Management
            // Route.CreateLoginPage(this);
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        void resizeMenuPanel()
         {
-           /* if (panel2.Width ==200)
+            DisplayPanel.Width = (MenuListPanel.Width == 200) ? DisplayPanel.Width + 195 : DisplayPanel.Width - 195;
+            DisplayPanel.Location = (MenuListPanel.Width == 200) ? new Point(DisplayPanel.Location.X - 195, DisplayPanel.Location.Y) : new Point(DisplayPanel.Location.X + 195, DisplayPanel.Location.Y);
+            MenuListPanel.Width = (MenuListPanel.Width == 200) ? 0 : 200;
+            if (MenuListPanel.Width == 200)
             {
-            panel2.Width = 50;
+                //showlist
             }
             else
             {
-                panel2.Width = 200;
+                //hideist show icon
+            }
+        }
+
+        private void MenuListBtn_Click(object sender, EventArgs e)
+        {
+            resizeMenuPanel();
+           /* foreach (Control c in ((Control)MenuListPanel).Controls)
+            {
+                if (c is Button)
+                {
+                    
+                        Console.WriteLine("Name: " + c.Name + "  Back Color: " + c.BackColor);
+                    
+                }
             }*/
-            panel3.Width = (panel2.Width == 200) ? panel3.Width+150 : panel3.Width-150;
-            panel3.Location = (panel2.Width == 200) ?  new Point(panel3.Location.X-150, panel3.Location.Y): new Point(panel3.Location.X + 150, panel3.Location.Y);
-            panel2.Width = (panel2.Width == 200) ? 50 : 200;
+        }
+
+        private void MarketingBtn_Click(object sender, EventArgs e)
+        {
+            ClickData = (ClickData== "Marketing") ?"" : "Marketing";
+            ShowSubMenu();
+        }
+        void ShowSubMenu()
+        {
+            CloseAllSubMenu();
+            if (ClickData == "Marketing")
+            {
+                CreateProductMktBtn.Show();
+                CreatePurchaseMktBtn.Show();
+            }
+            else if (ClickData == "WareHouse")
+            {
+                ReceiptionWhBtn.Show();
+                RequisitionWhBtn.Show();
+            }
+            else if (ClickData == "Packing")
+            {
+                OrderPkBtn.Show();
+                RequisitionPkBtn.Show();
+            }
+            else if (ClickData == "Shipping")
+            {
+                OrdersSpBtn.Show();
+            }
+            else if (ClickData == "Hr")
+            {
+                UserlistHrBtn.Show();
+            }
+            else if (ClickData == "Finance")
+            {
+                ExpensesFnBtn.Show();
+                RevenueFnBtn.Show();
+            }
+            else if (ClickData == "Purchasing")
+            {
+                PurchasePcBtn.Show();
+                ReceiptionPcBtn.Show();
+            }
+            else if(ClickData=="") 
+            {
+                CloseAllSubMenu();
+            }
+        }
+        void CloseAllSubMenu()
+        {
+            OrdersSpBtn.Hide();
+            ExpensesFnBtn.Hide();
+            RevenueFnBtn.Hide();
+            PurchasePcBtn.Hide();
+            ReceiptionPcBtn.Hide();
+            CreateProductMktBtn.Hide();
+            CreatePurchaseMktBtn.Hide();
+            ReceiptionWhBtn.Hide();
+            RequisitionWhBtn.Hide();
+            OrderPkBtn.Hide();
+            UserlistHrBtn.Hide();
+            RequisitionPkBtn.Hide();
+        }
+
+        private void WarehouseBtn_Click(object sender, EventArgs e)
+        {
+            ClickData = (ClickData == "WareHouse") ? "" : "WareHouse";
+            ShowSubMenu();
+        }
+
+        private void PackingBtn_Click(object sender, EventArgs e)
+        {
+            ClickData = (ClickData == "Packing") ? "" : "Packing";
+            ShowSubMenu();
+        }
+
+        private void PurchasingBtn_Click(object sender, EventArgs e)
+        {
+            ClickData = (ClickData == "Purchasing") ? "" : "Purchasing";
+            ShowSubMenu();
+        }
+
+        private void FinanceBtn_Click(object sender, EventArgs e)
+        {
+            ClickData = (ClickData == "Finance") ? "" : "Finance";
+            ShowSubMenu();
+        }
+
+        private void HrBtn_Click(object sender, EventArgs e)
+        {
+            ClickData = (ClickData == "Hr") ? "" : "Hr";
+            ShowSubMenu();
+        }
+
+        private void ShippingBtn_Click(object sender, EventArgs e)
+        {
+            ClickData = (ClickData == "Shipping") ? "" : "Shipping";
+            ShowSubMenu();
         }
     }
 }
